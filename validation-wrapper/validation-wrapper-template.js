b32dfd2a8e68a7014999681b2c1d23d63d717c3c
James Face <jamesfaceface@live.co.uk>
2017-03-08 12:56:26 +0000

    Initial version of validation wrapper

---
+++ validation-wrapper/validation-wrapper-template.js
@@ -0,0 +1,11 @@
+/**
+ * @license validation-wrapper
+ * Copyright 2017 @jamesfaceface https://github.com/jamesfaceface
+ * License: MIT
+ */
+
+angular.module('validationWrapper').run(['$templateCache', function($templateCache) {
+	$templateCache.put('validation-wrapper/validation-wrapper-template.html',
+		'<div ng-controller="ValidationWrapperCtrl as vm">	<div class="validation-message-label">		<ng-transclude ng-transclude-slot="labelElement"></ng-transclude>		<div class="alert alert-danger pull-right validation-message field-validation-error"			data-valmsg-for="{{vm.fieldName}}"			data-valmsg-replace="true"			ng-show="vm.shouldShowMessage()">			<span ng-repeat="validationMessage in vm.validationMessages" ng-show="vm.shouldShowMessage(validationMessage.type)">{{validationMessage.message}}</span>		</div>	</div>	<div class="validation-message-clearfix">		<ng-transclude ng-transclude-slot="inputElement"></ng-transclude>	</div></div>'
+	);
+}]);
